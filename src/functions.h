27a745d4a81aa0759eccde0f5cac5ff6d7245f2d
ViktorMakhnakov <72988870+ViktorMakhnakov@users.noreply.github.com>
2020-10-28 18:45:24 +0700

    Create functions.h

---
+++ src/functions.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+
+int ksum(int* array, int kol);
+int kseries(int* array, int kol);
+int selectSort(int* array, int kol);
+int soapSort(int* array, int kol);
+int shakeSort(int* array, int kol);
+int randomArray(int *array, int kol, int ks)
+
+#endif
