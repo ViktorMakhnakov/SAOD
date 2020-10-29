40809879bf8de37ff2673379efde52341dc5cb9d
ViktorMakhnakov <vityushka97@mail.ru>
2020-10-29 16:25:15 +0700

    new test

--- test/mytests.c
+++ test/mytests.c
@@ -132,7 +132,7 @@ CTEST(decr_array_6_elems, shake_moves2) {
 CTEST(array_1_elem, shake_moves3) { 
     int a[1] = {-852};
     const int result = shakeSort(a, 1); 
-    const int expected = 0; 
+    const int expected = 5; 
     ASSERT_EQUAL(expected, result); 
 }
 
@@ -144,88 +144,3 @@ CTEST(rand_array_10_elems, shake_moves4) {
     ASSERT_INTERVAL(min_expected, max_expected, result);
     system("PAUSE"); 
 }
-
-/*CTEST(rand_array_10_elems, select_sort1) { 
-    int a[10] = {87, -82, 126, -695, -95, -154, 46, 76, 12, 0};
-    shakeSort(a, 10); 
-    int b[10] = {-695, -154, -95, -82, 0, 12, 46, 76, 87, 126};
-    ASSERT_DATA(b, 10, a, 10); 
-}
-
-CTEST(incr_array_6_elems, select_sort2) { 
-    int a[6] = {-12, -6, 1, 8, 15, 28};
-    shakeSort(a, 6); 
-    int b[6] = {-12, -6, 1, 8, 15, 28};
-    ASSERT_DATA(b, 6, a, 6); 
-}
-
-CTEST(decr_array_8_elems, select_sort3) { 
-    int a[8] = {452, 384, 258, 184, 82, 22, -16, -154};
-    shakeSort(a, 8); 
-    int b[8] = {-154, -16, 22, 82, 184, 258, 384, 452};
-    ASSERT_DATA(b, 8, a, 8); 
-}
-
-CTEST(array_1_elem, select_sort4) { 
-    int a[1] = {95};
-    shakeSort(a, 1); 
-    int b[1] = {95};
-    ASSERT_DATA(b, 1, a, 1); 
-}
-
-CTEST(rand_array_6_elems, soap_sort1) { 
-    int a[6] = {68, 24, 466, -335, -2, 846};
-    soapSort(a, 6); 
-    int b[6] = {-335, -2, 24, 68, 466, 846};
-    ASSERT_DATA(b, 6, a, 6); 
-}
-
-CTEST(incr_array_8_elems, soap_sort2) { 
-    int a[8] = {-358, -286, -143, -54, 5, 25, 184, 295};
-    soapSort(a, 8); 
-    int b[8] = {-358, -286, -143, -54, 5, 25, 184, 295};
-    ASSERT_DATA(b, 8, a, 8); 
-}
-
-CTEST(decr_array_5_elems, soap_sort3) { 
-    int a[5] = {756, 657, 521, 317, 2};
-    soapSort(a, 5); 
-    int b[5] = {2, 317, 521, 657, 756};
-    ASSERT_DATA(b, 5, a, 5); 
-}
-
-CTEST(array_1_elem, soap_sort4) { 
-    int a[1] = {7};
-    soapSort(a, 1); 
-    int b[1] = {7};
-    ASSERT_DATA(b, 1, a, 1); 
-}
-
-CTEST(rand_array_10_elems, shake_sort1) { 
-    int a[10] = {59, -98, 48, 965, -171, -65, -254, 18, 236, -1};
-    shakeSort(a, 10); 
-    int b[10] = {-254, -171, -98, -65, -1, 18, 48, 59, 236, 965};
-    ASSERT_DATA(b, 10, a, 10); 
-}
-
-CTEST(incr_array_8_elems, shake_sort2) { 
-    int a[8] = {-125, -56, -43, -18, 0, 26, 35, 69};
-    shakeSort(a, 8); 
-    int b[8] = {-125, -56, -43, -18, 0, 26, 35, 69};
-    ASSERT_DATA(b, 8, a, 8); 
-}
-
-CTEST(decr_array_6_elems, shake_sort3) { 
-    int a[6] = {834, 654, 315, 155, 22, 7};
-    shakeSort(a, 6); 
-    int b[6] = {7, 22, 155, 315, 654, 834};
-    ASSERT_DATA(b, 6, a, 6); 
-}
-
-CTEST(array_1_elem, shake_sort4) { 
-    int a[1] = {-9};
-    shakeSort(a, 1); 
-    int b[1] = {-9};
-    ASSERT_DATA(b, 1, a, 1); 
-    system("PAUSE");
-}*/
